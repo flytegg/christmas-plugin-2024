e565868164fd0f79404d4471976786e193c62bb1
rolyPolyVole <yezhanting@gmail.com>
2024-12-09 10:37:19 +0000

    Format code

--- src/main/kotlin/gg/flyte/christmas/minigame/games/Spleef.kt
+++ src/main/kotlin/gg/flyte/christmas/minigame/games/Spleef.kt
@@ -21,6 +21,7 @@ import org.bukkit.Bukkit
 import org.bukkit.GameMode
 import org.bukkit.Material
 import org.bukkit.Sound
+import org.bukkit.block.Block
 import org.bukkit.block.data.type.Snow
 import org.bukkit.entity.ItemDisplay
 import org.bukkit.entity.Player
@@ -33,16 +34,19 @@ import org.bukkit.inventory.ItemStack
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 import org.bukkit.util.Vector
+import java.util.UUID
 
 class Spleef : EventMiniGame(GameConfig.SPLEEF) {
     private var overviewTasks = mutableListOf<TwilightRunnable>()
+
     private var floorLevelBlocks: List<MapSinglePoint> = listOf(
         Util.fillArena(110, Material.SNOW_BLOCK),
         Util.fillArena(98, Material.SNOW_BLOCK),
-        Util.fillArena(86, Material.SNOW_BLOCK),
+        Util.fillArena(86, Material.SNOW_BLOCK)
     ).flatMap { it }
+
     private var seconds = 0
-    private var doubleJumps = mutableMapOf<Player, Int>()
+    private var doubleJumps = mutableMapOf<UUID, Int>()
 
     override fun startGameOverview() {
         super.startGameOverview()
@@ -56,19 +60,25 @@ class Spleef : EventMiniGame(GameConfig.SPLEEF) {
 
     override fun preparePlayer(player: Player) {
         player.formatInventory()
-        player.gameMode = GameMode.ADVENTURE
-        player.teleport(gameConfig.spawnPoints.random().randomLocation())
+
         ItemStack(Material.DIAMOND_SHOVEL).apply {
             itemMeta = itemMeta.apply {
                 displayName("<!i><game_colour>Snow Shovel!".style())
             }
         }.let { player.inventory.setItem(0, it) }
-        doubleJumps[player] = 3
+
+        player.gameMode = GameMode.ADVENTURE
+        player.teleport(gameConfig.spawnPoints.random().randomLocation())
+
+        doubleJumps[player.uniqueId] = 3
     }
 
     override fun startGame() {
         overviewTasks.forEach { it.cancel() }
-        for (point in floorLevelBlocks) point.block.type = Material.SNOW_BLOCK // reset after game overview
+
+        for (point in floorLevelBlocks) {
+            point.block.type = Material.SNOW_BLOCK
+        } // reset after game overview
 
         simpleCountdown {
             Util.runAction(PlayerType.PARTICIPANT) {
@@ -76,51 +86,35 @@ class Spleef : EventMiniGame(GameConfig.SPLEEF) {
                 it.allowFlight = true
             }
 
+            addActionBarTask()
+
             tasks += repeatingTask(20) {
                 seconds++
-                updateScoreboard()
-                // every 30 seconds, they get points
+
                 if (seconds % 20 == 0) {
                     remainingPlayers().forEach {
                         it.sendMessage("<green>+1 point for surviving 30 seconds!".style())
                         eventController().addPoints(it.uniqueId, 1)
                     }
                 }
+
+                updateScoreboard()
             }
 
             tasks += repeatingTask(5) {
                 delay((1..15).random()) {
-                    // decay random snow blocks, that are not air
                     floorLevelBlocks.filter { it.block.type != Material.AIR }.random().let {
-                        when (it.block.type) {
-                            Material.SNOW_BLOCK -> {
-                                it.block.type = Material.SNOW
-                                it.block.blockData as? Snow ?: return@delay
-                                it.block.blockData = (it.block.blockData as Snow).apply { layers = 5 }
-                            }
-
-                            Material.SNOW -> {
-                                val blockData = it.block.blockData as Snow
-                                if (blockData.layers == 2) {
-                                    it.block.type = Material.AIR
-                                } else {
-                                    blockData.layers -= 3
-                                    it.block.blockData = blockData
-                                }
-                            }
-
-                            else -> return@delay
-                        }
+                        wearDownSnowBlock(it.block)
                     }
                 }
             }
-
-            manageActionBars()
         }
     }
 
     override fun eliminate(player: Player, reason: EliminationReason) {
-        Util.runAction(PlayerType.PARTICIPANT, PlayerType.OPTED_OUT) { it.sendMessage("<red>${player.name} <grey>has been eliminated!".style()) }
+        Util.runAction(PlayerType.PARTICIPANT, PlayerType.OPTED_OUT) {
+            it.sendMessage("<red>${player.name} <grey>has been eliminated!".style())
+        }
 
         player.apply {
             allowFlight = false
@@ -157,15 +151,15 @@ class Spleef : EventMiniGame(GameConfig.SPLEEF) {
 
         super.eliminate(player, reason)
 
-        val value = "$seconds second${if (seconds > 1) "s" else ""}"
+        val value = "$seconds second${if (seconds > 1) "s" else ""}" // bro imagine surviving a singular amount of seconds
         when (remainingPlayers().size) {
             1 -> {
-                formattedWinners.put(player.uniqueId, value)
-                formattedWinners.put(remainingPlayers().first().uniqueId, "$value (1st Place!)")
+                formattedWinners[player.uniqueId] = value
+                formattedWinners[remainingPlayers().first().uniqueId] = "$value (1st Place!)"
                 endGame()
             }
 
-            2 -> formattedWinners.put(player.uniqueId, value)
+            2 -> formattedWinners[player.uniqueId] = value
         }
     }
 
@@ -179,11 +173,13 @@ class Spleef : EventMiniGame(GameConfig.SPLEEF) {
         Bukkit.getOnlinePlayers().forEach { eventController().sidebarManager.updateLines(it, listOf(Component.empty(), playersLeft)) }
     }
 
-    private fun manageActionBars() {
+    private fun addActionBarTask() {
         tasks += repeatingTask(10) {
             remainingPlayers().forEach {
-                if (doubleJumps[it]!! > 0) {
-                    it.sendActionBar("<green><b>${doubleJumps[it]!!} <reset><game_colour>double jumps left!".style())
+                if (doubleJumps[it.uniqueId]!! > 0) {
+                    it.sendActionBar("<green><b>${doubleJumps[it.uniqueId]!!} <reset><game_colour>double jumps left!".style())
+                } else {
+                    it.sendActionBar("<red><b>0 <reset><game_colour>double jumps left!".style())
                 }
             }
         }
@@ -201,19 +197,22 @@ class Spleef : EventMiniGame(GameConfig.SPLEEF) {
                     })
                 }
             }
-            Bukkit.broadcastMessage("event got cancelled by this random ahh event");
         }
 
         listeners += event<PlayerToggleFlightEvent> {
-            if (player.gameMode != GameMode.SURVIVAL) return@event // elimed players alr in ADVENTURE
+            if (player.gameMode != GameMode.SURVIVAL) return@event
             isCancelled = true
 
-            if (doubleJumps[player]!! > 0) {
+            if (doubleJumps[player.uniqueId]!! > 0) {
                 player.isFlying = false
                 player.velocity = player.location.direction.multiply(0.5).add(Vector(0.0, 1.25, 0.0))
+
                 player.playSound(Sound.ITEM_TOTEM_USE)
-                doubleJumps[player] = doubleJumps[player]!! - 1
-            } else player.allowFlight = false
+
+                doubleJumps[player.uniqueId] = doubleJumps[player.uniqueId]!! - 1
+            } else {
+                player.allowFlight = false
+            }
         }
 
         listeners += event<ProjectileHitEvent> {
@@ -221,32 +220,43 @@ class Spleef : EventMiniGame(GameConfig.SPLEEF) {
             if (entity !is Snowball) return@event
 
             floorLevelBlocks.any { it.block == hitBlock }.let {
-                when (hitBlock!!.type) {
-                    Material.SNOW_BLOCK -> {
-                        hitBlock!!.type = Material.SNOW
-                        hitBlock!!.blockData as? Snow ?: return@event
-                        hitBlock!!.blockData = (hitBlock!!.blockData as Snow).apply { layers = 5 }
-                    }
-
-                    Material.SNOW -> {
-                        val blockData = hitBlock!!.blockData as Snow
-                        if (blockData.layers == 2) {
-                            hitBlock!!.type = Material.AIR
-                        } else {
-                            blockData.layers -= 3
-                            hitBlock!!.blockData = blockData
-                        }
-                    }
-
-                    else -> return@event
-                }
+                wearDownSnowBlock(hitBlock!!)
             }
         }
 
         listeners += event<PlayerMoveEvent> {
             if (player.location.blockY < 70) {
-                if (remainingPlayers().contains(player)) eliminate(player, EliminationReason.ELIMINATED)
+                if (remainingPlayers().contains(player)) {
+                    eliminate(player, EliminationReason.ELIMINATED)
+                }
             }
+        }
+    }
+
+    private fun wearDownSnowBlock(block: Block) {
+        when (block.type) {
+            Material.SNOW_BLOCK -> {
+                block.type = Material.SNOW
+
+                if (block.blockData !is Snow) {
+                    return
+                }
+
+                block.blockData = (block.blockData as Snow).apply { layers = 5 }
+            }
+
+            Material.SNOW -> {
+                val blockData = block.blockData as Snow
+
+                if (blockData.layers == 2) {
+                    block.type = Material.AIR
+                } else {
+                    blockData.layers -= 3
+                    block.blockData = blockData
+                }
+            }
+
+            else -> return
         }
     }
 
@@ -260,11 +270,11 @@ class Spleef : EventMiniGame(GameConfig.SPLEEF) {
 
     private fun incrementDoubleJumpsForAll(name: String?) {
         remainingPlayers().forEach {
-            doubleJumps[it] = doubleJumps[it]!! + 1
+            doubleJumps[it.uniqueId] = doubleJumps[it.uniqueId]!! + 1
             it.allowFlight = true
 
             if (name != null) {
-                it.sendMessage("<green>+1 double jump! (<aqua>$name</aqua>'s donation)".style())
+                it.sendMessage("<green>+1 double jump! (<aqua>$name's</aqua> donation)".style())
             } else {
                 it.sendMessage("<green>+1 double jump! (donation)".style())
             }
