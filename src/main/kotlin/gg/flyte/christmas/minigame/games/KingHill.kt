6ce92b8bec1d9569357d194c1dfb65a158d26c41
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-16 20:40:57 +0000

    debug: log end of KB period

--- src/main/kotlin/gg/flyte/christmas/minigame/games/KingHill.kt
+++ src/main/kotlin/gg/flyte/christmas/minigame/games/KingHill.kt
@@ -108,8 +108,12 @@ class KingHill : EventMiniGame(GameConfig.KING_OF_THE_HILL) {
                 }
 
                 if (delayedKbTicksLeft == 0) {
+                    println()
+                    println("Delayed KB period ended")
+                    println("Disabling delayed KB period by setting ticks left to -1")
                     delayedKbTicksLeft = -1
 
+                    println("Delayed KB ticks total: $delayedKbTicksTotal")
                     thrownAroundTicksLeft = delayedKbTicksTotal
                     thrownAroundTicksTotal = delayedKbTicksTotal
                     delayedKbTicksTotal = 0
